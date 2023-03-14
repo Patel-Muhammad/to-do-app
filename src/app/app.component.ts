587605a96ff1aaa9c671437fcea7543e78b09e51
Patel-Muhammad <patelmuh2003@gmail.com>
2023-03-15 01:00:11 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
   title = 'to-do-app';
+  initial = '';
+  tasks: TaskClass[]=[
+    new TaskClass("Go to gym"),
+    new TaskClass("Sleep"),
+    new TaskClass("angular tutorial")
+  ]
+  
+
+  addTask(taskitem:string){
+    if(taskitem == ''){
+      return
+    }
+    this.tasks.push(new TaskClass(taskitem))
+    this.initial = ''
+  }
+  deleteTask(taskitem:TaskClass){
+    if(taskitem.isDone){
+      this.tasks = this.tasks.filter(task=>task != taskitem)
+    }
+    else{
+      var isConfirm = confirm("Are you want to delete: " + `"${taskitem.title}"`)
+      if(isConfirm){
+        this.tasks = this.tasks.filter(task=>task != taskitem)
+      }
+    }
+  }
+  doneTask(taskitem:TaskClass){
+    taskitem.isDone = !taskitem.isDone;
+  }
+
+  isEnter(e:any){
+    if(e.keyCode == 13){
+      this.addTask(this.initial)
+    }
+  }
+
+  
+}
+class TaskClass{
+  constructor(public title:string){
+    
+  }
+  isDone = false;
 }
